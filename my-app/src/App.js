8e2e0c5df58336023ef0a31394c88f1fa78131d2
Lover1975 <drjoenh1983@gmail.com>
2024-07-10 10:25:12 -0700

    Add Signup page component
    - Create Signup.js component with a simple 'Hello World' message
    - Update App.js to include and render the Signup component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,14 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Signup from './signup';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Signup />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
