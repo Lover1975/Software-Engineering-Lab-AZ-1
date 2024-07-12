3ba0ff4936b031873a9c599978ba6d12a8365127
Lover1975 <drjoenh1983@gmail.com>
2024-07-12 11:42:42 -0700

    feat: Expand Profile component with additional elements
    - Added a welcoming paragraph
    - Included a button with an onClick event handler
    - Added an unordered list with three profile items
    - Included an additional paragraph for more content

--- my-app/src/Profile.js
+++ my-app/src/Profile.js
@@ -4,6 +4,14 @@ const Profile = () => {
   return (
     <div>
       <h1>Hello World!</h1>
+      <p>Welcome to the profile page.</p>
+      <button onClick={() => alert('Button clicked!')}>Click Me</button>
+      <ul>
+        <li>Profile Item 1</li>
+        <li>Profile Item 2</li>
+        <li>Profile Item 3</li>
+      </ul>
+      <p>This is a simple profile page created using React.</p>
     </div>
   );
 };
